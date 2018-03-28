8ba91a0a4c7024a8077f93f17eb3f0e2c858ec7c
Andrew Bryner <bryner.and@gmail.com>
2018-03-28 17:33:42 -0400

    added level objects

--- src/app.js
+++ src/app.js
@@ -1,23 +1,56 @@
 const game = {
   init() {
-    eventHandlers: {
-      $('button').on('click', game.helperFunctions.hideOverlay);
-      $('div').on('click', game.helperFunctions.sayHello);
-      $('.game-page').on('click', game.helperFunctions.showOverlay);
-      $('.timer').on('click', game.helperFunctions.decrimentTimer);
-      $('.row div').on('click', game.builder.placeObjects);
-      $('.bottle').on('click', game.builder.removeObjects);
-    }
+    $('button').on('click', game.helperFunctions.hideOverlay);
+    $('div').on('click', game.helperFunctions.sayHello);
+    $('.game-page').on('click', game.helperFunctions.showOverlay);
+    $('.timer').on('click', game.helperFunctions.decrimentTimer);
+    $('.row div').on('click', game.builder.placeObjects);
+    $('.bottle').on('click', game.builder.removeObjects);
+    $('.score').on('click', game.levelInfo.levelOne.loadLevel);
+  },
+  levelInfo: {
+    levelOne: {
+      {levelTime: 15,
+      cans: 10,
+      bottles: 5,
+      totalObjects: 15,
+      levelLocations: [],
+      loadLevel() {
+          for(let i = 0; i < this.totalObjects; i++) {
+            let newNums = [];
+            let num = game.helperFunctions.getRandomInt();
+            if (jQuery.inArray(num, newNums) === -1) {
+              newNums.push(num);
+            }
+            this.levelLocations = newNums;
+          }
+        console.log(this.levelLocations);
+        },
+      },
+    levelTwo: {
+      cans: 20,
+      bottles: 10,
+      totalObjects: 30,
+    },
+    levelThree: {
+      cans: 34,
+      bottles: 20,
+      totalObjects: 54,
+    },
   },
   builder: {
-    placeObjects(e) {
-      $(this).addClass('bottle');
+    placeObjects() {
+      $(this).addClass('can');
     },
-    removeObjects(e) {
-      $(this).removeClass('bottle');
-    }
+    removeObjects() {
+      $(this).remove('.bottle');
+    },
   },
   helperFunctions: {
+    getRandomInt() {
+      const num = Math.floor(Math.random() * (101 - 1)) + 1;
+      return num;
+    },
     hideOverlay(e) {
       $('.landing-page').css('visibility', 'hidden');
       e.stopPropagation();
@@ -30,13 +63,13 @@ const game = {
       console.log('div clicked');
       e.stopPropagation();
     },
-    decrimentTimer(e, levelTime) {
+    decrimentTimer(e) {
       let time = 12;// levelTime
-      $('.timer span').html(time + " seconds");
+      $('.timer span').html(time);
       function tic() {
         if (time > 0) {
           time -= 1;
-          $('.timer span').html(time + " seconds");
+          $('.timer span').html(time);
         } else {
           alert('time is up!');
           clearInterval(timer);
@@ -45,7 +78,7 @@ const game = {
       let timer = setInterval(tic, 1000);
       e.stopPropagation();
     },
-  }
+  },
 };
 $(document).ready(function(){
   game.init();
