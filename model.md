107a0b8696dc71d45eddceaf623a46b6b2e16292
Andrew Bryner <bryner.and@gmail.com>
2018-03-28 15:33:49 -0400

    added timer, board, and overlays

--- model.md
+++ model.md
@@ -11,6 +11,13 @@ A screen signalling the start of a round. countdown? beep beep BEEEP. 3,2,1, GO!
 repeat...
 On completion of level 3. A win screen pops up. you can retry which will begin the process anew from lvl 1
 
+win logic: if level array matches player array & timer has not reached 0 then, next level
+    increment level
+        if level array matches player array & timer has not reached 0 then, next level
+    increment level
+        if level array matches player array & timer has not reached 0 then, you win
+    display win page
+
 
 HTML implementation => View
 landing page > overlay,
@@ -47,5 +54,6 @@ user interaction => Controller
 landing page >> click start game button, *click button to navigate to high score page
 game page >> click targets
 failure page >> click to restart game
-win page >> click to restart game, *form to enter high score data. on completion of form, shows high score page
+win page >> click to restart game, *form to enter high score data. on completion of form, shows high score page 
+high score page >> click to restart game
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,52 @@
-let x = 0;
-
-function xasdf(a) {
-  x = a;
-  console.log(x);
-}
-xasdf(9);
+const game = {
+  init() {
+    eventHandlers: {
+      $('button').on('click', game.helperFunctions.hideOverlay);
+      $('div').on('click', game.helperFunctions.sayHello);
+      $('.game-page').on('click', game.helperFunctions.showOverlay);
+      $('.timer').on('click', game.helperFunctions.decrimentTimer);
+      $('.row div').on('click', game.builder.placeObjects);
+      $('.bottle').on('click', game.builder.removeObjects);
+    }
+  },
+  builder: {
+    placeObjects(e) {
+      $(this).addClass('bottle');
+    },
+    removeObjects(e) {
+      $(this).removeClass('bottle');
+    }
+  },
+  helperFunctions: {
+    hideOverlay(e) {
+      $('.landing-page').css('visibility', 'hidden');
+      e.stopPropagation();
+    },
+    showOverlay(e) {
+      $('.final-screen').css('visibility', 'visible');
+      e.stopPropagation();
+    },
+    sayHello(e) {
+      console.log('div clicked');
+      e.stopPropagation();
+    },
+    decrimentTimer(e, levelTime) {
+      let time = 12;// levelTime
+      $('.timer span').html(time + " seconds");
+      function tic() {
+        if (time > 0) {
+          time -= 1;
+          $('.timer span').html(time + " seconds");
+        } else {
+          alert('time is up!');
+          clearInterval(timer);
+        }
+      }
+      let timer = setInterval(tic, 1000);
+      e.stopPropagation();
+    },
+  }
+};
+$(document).ready(function(){
+  game.init();
+});
