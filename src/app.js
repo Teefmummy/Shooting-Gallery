28182573cedfd07d10bce7957f4e97d6f5e573d5
Andrew Bryner <bryner.and@gmail.com>
2018-03-31 14:55:26 -0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const game = {
   init() {
-    // click the start button to trigger level 1
-    $('#game-start').on('click', game.helperFunctions.hideOverlay);
+    // set event handlers
     $('#game-start').on('click', function(e) {
+      game.helperFunctions.hideOverlay();
       game.levelBuilder(game.levels[0]);
       e.preventDefault();
     });
-    $('.row div').on('click', '.clickable', game.playerFunctions.shootTargets);
-    $('.restart').on('click', game.helperFunctions.restartGame);
-    $('.next').on('click', game.helperFunctions.hideOverlay);
+    $('.row div').on('click', '.clickable', game.shootTargets);
+    $('.restart').on('click', game.restartGame);
     $('.next').on('click', function(e) {
+      game.helperFunctions.hideOverlay();
       game.levelBuilder(game.levels[game.playerInfo.currentLevel]);
       e.preventDefault();
     });
@@ -26,21 +26,23 @@ const game = {
       cans: 10,
       bottles: 5,
       totalObjects: 15,
+      points: 30,
     },
     {
       time: 12,
       cans: 15,
       bottles: 10,
       totalObjects: 25,
+      points: 50,
     },
     {
       time: 15,
       cans: 20,
       bottles: 10,
       totalObjects: 30,
+      points: 60,
     },
   ],
-
   levelBuilder(level) {
     // set variables for use in builder
     let cans = level.cans;
@@ -54,96 +56,99 @@ const game = {
     // load the locations
     function loadLevel() {
       // $('.landing-page').css('visibility', 'hidden');
-        let newNums = [];
-          while(levelLocations.length < totalObjects) {
-            let num = game.helperFunctions.getRandomInt();
-            if (jQuery.inArray(num, newNums) === -1) {
-              newNums.push(num);
-            }
-            levelLocations = newNums;
-          }
+      const newNums = [];
+      while (levelLocations.length < totalObjects) {
+        const num = game.helperFunctions.getRandomInt();
+        if (jQuery.inArray(num, newNums) === -1) {
+          newNums.push(num);
         }
+        levelLocations = newNums;
+      }
+    }
     // set and start the timer
     function setTimer() {
       let time = level.time;// levelTime
       $('.timer span').html(time);
+      const timer = setInterval(tic, 1000);
       function tic() {
         if (time > 0) {
           time -= 1;
           $('.timer span').html(time);
         } else {
           clearInterval(timer);
-          game.playerInfo.currentLevel += 1;
-          game.nextLevel();
+          game.displayScreen();
         }
       }
-      let timer = setInterval(tic, 1000);
 
     }
     // place the objects
     function placeObjects() {
-        for(let i = 0; i < levelLocations.length; i += 1){
-          if (bottles > 0){
-            $(`.game-board .row .${levelLocations[i]}`).html('<div class=\'bottle clickable\' data=\'5\'></div>');
-            bottles -= 1;
-          } else if (cans > 0){
-            $(`.game-board .row .${levelLocations[i]}`).html('<div class=\'can clickable\' data=\'1\'></div>');
-            cans -= 1;
-          }
+      for (let i = 0; i < levelLocations.length; i += 1) {
+        if (bottles > 0) {
+          $(`.game-board .row .${levelLocations[i]}`).html('<div class=\'bottle clickable\' data=\'5\'></div>');
+          bottles -= 1;
+        } else if (cans > 0) {
+          $(`.game-board .row .${levelLocations[i]}`).html('<div class=\'can clickable\' data=\'1\'></div>');
+          cans -= 1;
         }
       }
+    }
     // call the functions
     loadLevel();
     setTimer();
     placeObjects();
   },
-  nextLevel(){
-    if (game.playerInfo.currentLevel === 3 && game.playerInfo.currentLevelScore > 30){
-      game.helperFunctions.showFinal();
-    } else if (game.playerInfo.currentLevel < 3 && game.playerInfo.currentLevelScore > 30){
-      game.helperFunctions.levelCompleteScreen();
+  displayScreen() {
+    const $levelComplete = $('.level-complete-screen');
+    const $finalScreen = $('.final-screen');
+    const $points = $('.final-score span');
+    const $winSpan = $('.win span');
+    if (game.playerInfo.currentLevel < 2) {
+      if (game.playerInfo.currentLevelScore > game.levels[game.playerInfo.currentLevel].points) {
+        $levelComplete.css('visibility', 'visible');
+        $points.html(game.playerInfo.currentLevelScore);
+        $winSpan.html('Win!');
+        game.playerInfo.currentLevel += 1;
+      } else {
+        $finalScreen.css('visibility', 'visible');
+        $points.html(game.playerInfo.currentLevelScore);
+        $winSpan.html('Lose!');
+      }
     } else {
-      game.helperFunctions.showFinal();
+      if (game.playerInfo.currentLevelScore > game.levels[game.playerInfo.currentLevel].points) {
+        $finalScreen.css('visibility', 'visible');
+        $points.html(game.playerInfo.totalScore);
+        $winSpan.html('Win!');
+      } else {
+        $finalScreen.css('visibility', 'visible');
+        $points.html(game.playerInfo.currentLevelScore);
+        $winSpan.html('Lose!');
+      }
     }
   },
-  playerFunctions: {
-    shootTargets(e) {
-      let points = $(this).attr('data');
-      game.playerInfo.totalScore += parseInt(points);
-      game.playerInfo.currentLevelScore += parseInt(points);
-      $('.score span').html(game.playerInfo.currentLevelScore);
-      $(this).remove();
-    },
+  restartGame() {
+    game.playerInfo.totalScore = 0;
+    game.playerInfo.currentLevel = 0;
+    game.helperFunctions.hideOverlay();
+    $('.clickable').remove();
+    game.levelBuilder(game.levels[0]);
+  },
+  shootTargets() {
+    const points = $(this).attr('data');
+    game.playerInfo.totalScore += parseInt(points, 10);
+    game.playerInfo.currentLevelScore += parseInt(points, 10);
+    $('.score span').html(game.playerInfo.currentLevelScore);
+    $(this).remove();
   },
   helperFunctions: {
     getRandomInt() {
       const num = Math.floor(Math.random() * (51 - 1)) + 1;
       return num;
     },
-    levelCompleteScreen() {
-      $('.level-complete-screen .final-score span').html(game.playerInfo.totalScore);
-      $('.level-complete-screen').css('visibility', 'visible');
-    },
     hideOverlay(e) {
       $('.landing-page').css('visibility', 'hidden');
       $('.final-screen').css('visibility', 'hidden');
       $('.level-complete-screen').css('visibility', 'hidden');
-    },
-    showFinal(e) {
-      $('.final-screen').css('visibility', 'visible');
-      if(game.playerInfo.totalScore >= 30){
-        $('.win span').html('Win!');
-      } else if (game.playerInfo.totalScore < 30){
-        $('.win span').html('Lose!');
-      }
-      $('.final-score span').html(game.playerInfo.totalScore);
-    },
-    restartGame() {
-      console.log('yo');
-      game.playerInfo.totalScore = 0;
-      game.helperFunctions.hideOverlay();
-      $('.clickable').remove();
-      game.levelBuilder(game.levels[0]);
     },
   },
 };
